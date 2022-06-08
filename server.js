85fd9e85a1695b1795ebf917075de990af618d56
nateThickett <92690338+nateThickett@users.noreply.github.com>
2022-06-08 14:48:49 -0700

    added specific calculator functionality and redirects

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ var app = express();
 
 
 
-
+var calculators = {
+   BMI: [ /**/ ],
+   BMRM: [ /**/ ],
+   BMRW: [ /**/ ],
+   HYPOT: [ /**/ ],
+ };
+ 
 
 app.engine('handlebars', exphbs.engine({defaultLayout: 'base'}));
 app.set('view engine', 'handlebars');
@@ -22,7 +28,25 @@ app.get('/', function(req, res) {
    res.status(200).sendFile(path.join(__dirname, '/public/calc.html'));
 });
 
+app.get('/calc', function(req, res) {
+   res.status(200).sendFile(path.join(__dirname, '/public/calc.html'));
+});
 
+app.get('/past-problems',(req,res)=>{
+   res.status(200).render('404');
+  });
+
+app.get('/calc/:calcName', function (req, res) {
+   var calcName = req.params.calcName;
+   if (calculators[calcName]) {
+      res.render('specific', {
+        // placeholder 
+      });
+    } else {
+      res.status(404).render('404');
+    }
+  
+});
 
 
 app.get('*', function (req, res) {
@@ -32,3 +56,4 @@ app.get('*', function (req, res) {
 app.listen(port, function() {
    console.log("------- Server is running on port :", port);
 });
+
